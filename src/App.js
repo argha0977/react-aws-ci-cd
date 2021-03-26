5a3760109cad787d0a367fa1c760ed6da5ad9415
Argha <email2argha@gmail.com>
2021-03-26 15:25:00 +0530

    Build spec added

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React CI/CD Test
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>V 0.0.1</p>
       </header>
     </div>
   );
